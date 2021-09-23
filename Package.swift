80e19b887ce5efa72a31d210950e42337c4939c8
Carlos E. Hernández <carlos.h@mac.com>
2021-09-22 23:38:50 -0700

    testing dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "SophiSDK",
-            targets: ["SophiSDKTracker"]),
+            targets: ["SophiSDKTrackerTargets"]),
     ],
     dependencies: [
         .package(name: "SnowplowTracker", url: "https://github.com/snowplow/snowplow-objc-tracker", from: "2.2.2")
